d1e8e7ec51892e86f21d4f8202df7622fb1105cc
andrew tsai <os-andrew.ja.tsai@twtp1pc1718.deltaos.corp>
2023-10-04 14:51:24 +0800

    Push project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deploy code from Kaggle competition for data-zoomcamp certification',
+    author='andrew',
+    license='MIT',
+)
