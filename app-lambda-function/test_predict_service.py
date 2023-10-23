412fff0821b4118be79764a39374bcc6da9deca1
andrew tsai <os-andrew.ja.tsai@twtp1pc1718.deltaos.corp>
2023-10-23 09:11:52 +0800

    Update

--- app-lambda-function/test_predict_service.py
+++ app-lambda-function/test_predict_service.py
@@ -4,6 +4,6 @@ url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
 url = 'https://x8ee6c4ds4.execute-api.ap-southeast-2.amazonaws.com/test/predict'
 
-data = {'data': [1,2,3,4]}
+data = {'data': [1,1,1,1]}
 
 print(requests.post(url, json=data).json())
